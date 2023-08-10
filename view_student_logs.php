49c33ac06f63e51aaa68c6f10097b638c4c5579e
Varun_Triwits <129926996+Triwits-Varun@users.noreply.github.com>
2023-08-10 17:32:08 +0530

    Add files via upload

---
+++ view_student_logs.php
@@ -0,0 +1,64 @@
+
+
+<!-- Main Content-->
+<div class="main-content pt-5 mt-5">
+	<div class="container-fluid">
+
+		<!-- Page Header -->
+	<!-- 		<div class="text-wrap">
+			<div class="example">
+				<nav aria-label="breadcrumb">
+					<ol class="breadcrumb breadcrumb-style1 mg-b-0">
+
+						<li class="breadcrumb-item">
+							<a href="<?php echo base_url()?>">Support</a>
+						</li>
+							<li class="breadcrumb-item active">Webinar</li>
+					</ol>
+				</nav>
+			</div>
+		</div> -->
+		<!-- End Page Header -->
+
+
+
+		<!-- Row -->
+		<div class="row mt-5" style="text-align: center;">
+			
+			<div class="col-lg-6 col-md-4">
+				<a href="<?php echo base_url('student_login_logs')?>">
+					<div class="card plan-card">
+						<div class="card-block">
+							<div class="text-center">
+								<h1 class="pt-2"><i class="fe fe-users plan-icon bg-secondary"></i></h1>
+								<h6 class="text-uppercase font-weight-semibold text-secondary">Student Login Logs</h6>
+							</div>
+
+						</div>
+					</div>
+				</a>
+
+			</div>
+				
+
+			<div class="col-lg-6 col-md-4">
+				<a href="<?php echo base_url('student_scholarship_logs')?>">
+					<div class="card plan-card">
+						<div class="card-block">
+							<div class="text-center">
+								<h1 class="pt-2"><i class="fa fa-history   plan-icon bg-secondary"></i></h1>
+								<h6 class="text-uppercase font-weight-semibold text-secondary">Student Applied Scholarship</h6>
+							</div>
+									
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				
+			</div>
+
+		</div>
+		<!-- End Main Content-->
+
+
